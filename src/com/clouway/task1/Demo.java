5a7c752135802641625804c33d2d48a2093138a2
ivanP91 <ivan.peichev91@gmail.com>
2016-05-11 09:15:29 +0300

    ready with task1 from IO

---
+++ src/com/clouway/task1/Demo.java
@@ -0,0 +1,16 @@
+package com.clouway.task1;
+
+import java.io.IOException;
+
+/**
+ * Created by clouway on 09.05.16.
+ */
+public class Demo {
+  public static void main(String[] args) throws IOException {
+    ReadeConsole readeConsole = new ReadeConsole();
+    System.out.println(readeConsole.readeString());
+    System.out.println(readeConsole.readInt());
+    System.out.println(readeConsole.readChar());
+    System.out.println(readeConsole.readFloat());
+  }
+}
---
+++ src/com/clouway/task3/ConsoleWriteInTextFail.java
@@ -0,0 +1,7 @@
+package com.clouway.task3;
+
+/**
+ * Created by clouway on 10.05.16.
+ */
+public class ConsoleWriteInTextFail {
+}
