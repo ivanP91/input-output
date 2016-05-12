0e0debaed70a48eff7bf71d032dad5fa31c7a9a0
ivanP91 <ivan.peichev91@gmail.com>
2016-05-12 11:31:00 +0300

    Commit.

---
+++ src/com/clouway/task2/Demo.java
@@ -0,0 +1,12 @@
+package com.clouway.task2;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by clouway on 12.05.16.
+ */
+public class Demo {
+
+  }
+
---
+++ src/com/clouway/task2/InputOutputFile.java
@@ -0,0 +1,18 @@
+package com.clouway.task2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by clouway on 12.05.16.
+ */
+public class InputOutputFile {
+  public String file;
+  public InputOutputFile(String name) {
+    file = name;
+  }
+
+}
