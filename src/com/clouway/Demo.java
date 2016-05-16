4ee53b979a964b24184c86eaba985c06b18d70ff
ivanP91 <ivan.peichev91@gmail.com>
2016-05-16 15:05:24 +0300

    task three from IO is ready

---
+++ src/com/clouway/Demo.java
@@ -0,0 +1,19 @@
+package com.clouway;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Demo {
+  public static void main(String[] args) throws IOException {
+    System.out.print("name file: ");
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+    System.out.print("Enter breaker character ");
+    String breaker = sc.nextLine();
+    System.out.println("Write to file: ");
+    ReverseFile test = new ReverseFile(name, breaker);
+    test.write();
+    System.out.println("content will be reversed in reversedfile.txt");
+    test.reverse("reversedfile.txt");
+  }
+}
