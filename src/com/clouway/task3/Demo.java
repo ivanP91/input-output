557a3acd78d046bf88cbe82e69a4c2074917f6c9
ivanP91 <ivan.peichev91@gmail.com>
2016-05-16 15:21:36 +0300

    task three  from IO is ready

---
+++ src/com/clouway/task3/Demo.java
@@ -0,0 +1,22 @@
+package com.clouway.task3;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by clouway on 16.05.16.
+ */
+public class Demo {
+  public static void main(String[] args) throws IOException {
+    System.out.print("name file ");
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+    System.out.print("Enter breaker character");
+    String breaker = sc.nextLine();
+    System.out.print("Write to file:");
+    ReverseFile test = new ReverseFile(name, breaker);
+    test.write();
+    System.out.println("content will be reversed in reversedfile.txt");
+    test.reverse("reversedfile.txt");
+  }
+}
---
+++ src/com/clouway/task3/ReverseFile.java
@@ -0,0 +1,62 @@
+package com.clouway.task3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by clouway on 16.05.16.
+ */
+public class ReverseFile {
+  private String file;
+  private String breakCharacter;
+
+  public ReverseFile(String file, String breakCharacter) {
+    this.breakCharacter=breakCharacter;
+    this.file=file;
+  }
+
+  public void write() throws IOException {
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter out = new BufferedWriter((new FileWriter(file)));
+    try {
+      String line;
+      do {
+        line = in.readLine();
+        out.write(line);
+        out.newLine();
+      } while (!line.equals(breakCharacter));
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      out.close();
+      in.close();
+    }
+  }
+  public void reverse(String reverseFile) throws IOException {
+    BufferedReader input = new BufferedReader(new FileReader(file));
+    BufferedWriter output = new BufferedWriter(new FileWriter(reverseFile));
+    String line;
+    try {
+      do {
+        line = input.readLine();
+        StringBuilder buf = new StringBuilder(line);
+        output.write(String.valueOf(buf.reverse()));
+        output.newLine();
+      }
+      while (!line.equals(breakCharacter));
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      input.close();
+      output.close();
+    }
+  }
+}
+
+
+
+
